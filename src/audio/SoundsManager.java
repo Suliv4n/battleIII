d149e28a94f7e07d471be1ba22f569bb4823e9c0
Sulivan <sulivan.bochant@gmail.com>
2015-11-19 22:45:00 +0100

    Sounds Manager

---
+++ src/audio/SoundsManager.java
@@ -0,0 +1,28 @@
+package audio;
+
+import game.system.Configurations;
+
+import java.util.HashMap;
+
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Sound;
+
+
+
+
+
+public class SoundsManager
+{
+	
+	private static HashMap<String, Sound> sounds;
+	
+	public static void init() throws SlickException{
+		sounds = new HashMap<String, Sound>();
+		for(String name : new String[]{"select"})
+		sounds.put("select", new Sound(Configurations.SOUNDS_FOLDER+name+".ogg"));
+	}
+	
+	public static void play(String id){
+		sounds.get(id).play();
+	}
+}
