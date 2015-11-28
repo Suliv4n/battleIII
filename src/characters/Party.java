f3161c7e427ec389a12f1c47c68af3dcf9cdf6cf
Sulivan <sulivan.bochant@gmail.com>
2015-11-28 19:44:09 +0100

    Orientation avec des valeurs en puissance de 2

--- src/characters/Party.java
+++ src/characters/Party.java
@@ -38,10 +38,10 @@ import data.DataManager;
  */
 public class Party implements Iterable<Character>
 {
-	public static final int SOUTH = 0;
-	public static final int NORTH = 1;
-	public static final int EAST = 2;
-	public static final int WEST = 3;
+	public static final int SOUTH = 1;
+	public static final int NORTH = 2;
+	public static final int EAST = 4;
+	public static final int WEST = 8;
 	
 	
 	//COORDONNEES
